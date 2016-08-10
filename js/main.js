e93a09e91a5573b7b7acc666532b3aa2129dcdba
Adam Gavish <agavish@gmail.com>
2016-08-10 12:18:57 -0400

    markers

--- js/main.js
+++ js/main.js
@@ -194,28 +194,29 @@
 		} , { offset: '85%' } );
 	};
 
-		var initialize = function initialize() {
-		            var mapOptions = {
-		                center: new google.maps.LatLng(40.740898, -74.002181),
-		                zoom: 9,
-		                scrollwheel: false,
-		                draggable: true,
-		                panControl: true,
-		                zoomControl: true,
-		                mapTypeControl: true,
-		                scaleControl: true,
-		                streetViewControl: true,
-		                overviewMapControl: true,
-		                rotateControl: true,
-		                zoom: 15
-		            };
-		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-		        }
-		google.maps.event.addDomListener(window, 'load', initialize);
+		// var initialize = function initialize() {
+		//             var mapOptions = {
+		//                 center: new google.maps.LatLng(40.740898, -74.002181),
+		//                 query: 'verizon wireless',
+		//                 zoom: 9,
+		//                 scrollwheel: false,
+		//                 draggable: true,
+		//                 panControl: true,
+		//                 zoomControl: true,
+		//                 mapTypeControl: true,
+		//                 scaleControl: true,
+		//                 streetViewControl: true,
+		//                 overviewMapControl: true,
+		//                 rotateControl: true,
+		//                 zoom: 15
+		//             };
+		// var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		//         }
+		// google.maps.event.addDomListener(window, 'load', initialize);
 		$('.demo').supercal();
 
 	$(function(){
-		initialize();
+		// initialize();
 		fullHeight();
 		sliderMain();
 		centerBlock();
